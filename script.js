4496d6a49f8db768d4b406565b023c3ca3862782
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-04-27 00:02:43 +0200

    initial

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const checkBtn = document.querySelector('.check');
+const againBtn = document.querySelector('.again');
+const correctNumber = document.querySelector('.number');
+const guessNumber = document.querySelector('.guess');
+const score = document.querySelector('.score');
+const highScore = document.querySelector('.highscore');
